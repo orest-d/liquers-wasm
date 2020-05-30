013839517b32cd3539bba6e8461469d2776904ff
Orest Dubay <orest3.dubay@gmail.com>
2020-05-31 00:34:51 +0200

    registration prototype compiles, but untested

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 extern crate cfg_if;
+extern crate js_sys;
 extern crate wasm_bindgen;
 extern crate liquers_core;
 extern crate serde;
@@ -10,15 +11,22 @@ extern crate lazy_static;
 use liquers_core::value::*;
 use liquers_core::error::*;
 use liquers_core::action_registry::*;
-use liquers_core::query::Environment;
-use std::sync::{Mutex};
+use liquers_core::query::{Environment, ActionParameter};
+
+use std::sync::{Arc, Mutex};
 use std::convert::{TryFrom, TryInto};
+use std::cell::RefCell;
 
 //use cfg_if::cfg_if;
 use wasm_bindgen::prelude::*;
 
+/*
 lazy_static! {
     static ref REGISTRY:Mutex<HashMapActionRegistry<Value>> = Mutex::new(HashMapActionRegistry::<Value>::new());
+}
+*/
+thread_local!{
+    static REGISTRY : RefCell<HashMapActionRegistry<Value>> = RefCell::new(HashMapActionRegistry::<Value>::new());
 }
 
 #[wasm_bindgen]
@@ -29,9 +37,38 @@ extern {
 #[wasm_bindgen]
 pub fn register_hello() {
     use liquers_core::action_registry::*;
-    let registry = &mut *REGISTRY.lock().expect("Registry lock failed");    
-    let hello = |x:String| format!("Hello, {}!",x);
-    registry.register_callable_action("root", "hello", Box::new(Function1(Box::new(hello))));
+    //let registry = &mut *REGISTRY.lock().expect("Registry lock failed");
+    REGISTRY.with(
+        |registry|{
+            let hello = |x:String| format!("Hello, {}!",x);
+            (*registry).borrow_mut().register_callable_action("root", "hello", Box::new(Function1(Box::new(hello))));
+        }
+    );  
+}
+
+struct JsFunction(js_sys::Function);
+
+impl CallableAction<Value> for JsFunction{
+    fn call_action(&self, input:Value, _arguments:&Vec<ActionParameter>) -> Result<Value, Error>{
+        let f_input:JsValue = value_to_jsvalue(input);
+        let mut arguments = js_sys::Array::of1(&f_input);
+        let this = JsValue::NULL;
+
+        let out = self.0.apply(&this, &arguments).map_err(|e| Error::General{message:format!("Execution error: {:?}",e)})?;
+        let result = jsvalue_to_value(out)?;
+        Ok(result)
+    }
+}
+
+#[wasm_bindgen]
+pub fn register(ns:&str, name:&str, f: js_sys::Function) -> Result<JsValue,JsValue> {
+    use liquers_core::action_registry::*;
+    //let mut registry = REGISTRY.lock().map_err(|e| JsValue::from(format!("Registry lock failed: {}",e)))?;
+    //let name:&str = f.name().into();
+    REGISTRY.with(|registry|{
+        (*registry).borrow_mut().register_callable_action(ns, name, Box::new(JsFunction(f)));
+    });
+    Ok(JsValue::NULL)
 }
 
 pub fn jsvalue_to_value(jsvalue: JsValue) -> Result<Value, Error>{
@@ -67,10 +104,12 @@ pub fn value_to_jsvalue(value:Value)->JsValue{
 
 #[wasm_bindgen]
 pub fn eval(input:JsValue, query:&str) -> Result<JsValue,JsValue>{
-    let mut registry = REGISTRY.lock().map_err(|e| JsValue::from(format!("Registry lock failed: {}",e)))?;
-    
+    //let mut registry = REGISTRY.lock().map_err(|e| JsValue::from(format!("Registry lock failed: {}",e)))?;
     let input = jsvalue_to_value(input).map_err(|e| format!("{}",e))?;
-    let result = (*registry).eval(input,query).map_err(|e| format!("{}",e))?;
+    let result=REGISTRY.with(|registry|{
+        (*registry).borrow_mut().eval(input,query).map_err(|e| format!("{}",e))
+    })?;
+    //let result = (*registry).eval(input,query).map_err(|e| format!("{}",e))?;
     Ok(value_to_jsvalue(result))
 }
 
